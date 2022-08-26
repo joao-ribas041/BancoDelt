249bde03258aba061c554ce301c89a07f4f85a96
JoaoRibas041 <ribas.joao1410@gmail.com>
2022-08-25 23:21:43 -0300

    Inicio do projeto

---
+++ src/com/bancodelt/java/program/Main.java
@@ -0,0 +1,43 @@
+package com.bancodelt.java.program;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author João Ribas
+ */
+
+public class Main extends Application {
+    
+    private static Stage programLogin;
+    
+    private static final String directoryJavaInterfaces = "/com/bancodelt/java/program/interfaces/";
+    
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource(Main.getDirectoryJavaInterfaces() + "ViewOpenApp.fxml"));
+        
+        Scene scene = new Scene(root);
+        
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    // Método para acessar as "cenas" do programa
+    public static Stage getProgramLogin() {
+        return programLogin;
+    }
+    
+    // Método para obter os diretórios
+    public static String getDirectoryJavaInterfaces() {
+        return directoryJavaInterfaces;
+    }
+}
---
+++ src/com/bancodelt/java/program/controllers/ViewOpenAppController.java
@@ -0,0 +1,23 @@
+package com.bancodelt.java.program.controllers;
+
+import javafx.fxml.FXML;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+
+/**
+ *
+ * @author João Ribas
+ */
+public class ViewOpenAppController implements Initializable {
+    
+    @FXML
+    private Button btnEntrar;
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
