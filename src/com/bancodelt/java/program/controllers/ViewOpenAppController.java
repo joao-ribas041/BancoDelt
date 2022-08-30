044f7f099f05178460a8c3106b1d4911d5224dad
JoaoRibas041 <ribas.joao1410@gmail.com>
2022-08-29 22:49:10 -0300

    Botao de alerta funcionando

--- src/com/bancodelt/java/program/controllers/ViewOpenAppController.java
+++ src/com/bancodelt/java/program/controllers/ViewOpenAppController.java
@@ -1,23 +1,41 @@
 package com.bancodelt.java.program.controllers;
 
-import javafx.fxml.FXML;
+import com.bancodelt.java.models.alerts.AlertWarningPrototype;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.TextField;
 
-/**
- *
- * @author João Ribas
- */
 public class ViewOpenAppController implements Initializable {
-    
+
     @FXML
     private Button btnEntrar;
+    @FXML
+    private TextField txtFCPF;
     
+    AlertWarningPrototype alertaAviso = new AlertWarningPrototype();
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-    }    
+    } 
     
+    @FXML
+    private void AcaoBtnEntrar(ActionEvent event) {
+        CheckACC();
+    }
+    
+    private void CheckACC() {            
+        if (txtFCPF.getText().isEmpty()) {
+            alertaAviso.setTitulo("Alerta");
+            alertaAviso.setMsgPrincipal("Informe o CPF!");
+            alertaAviso.setSubMsg("O campo CPF esta vazio. Por favor informe o CPF");
+            alertaAviso.exibirAlert();
+        }
+    }
 }
