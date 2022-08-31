bea4b4cfdcca11f30878d6bb5f69038b236f2432
JoaoRibas041 <ribas.joao1410@gmail.com>
2022-08-30 23:23:43 -0300

    Novas variaveis nas classes conta
    Adicionado as variaveis: Email, ddd, numero do celular e alteração da data.

--- src/com/bancodelt/java/models/Conta.java
+++ src/com/bancodelt/java/models/Conta.java
@@ -1,15 +1,19 @@
 package com.bancodelt.java.models;
 
+import java.util.Date;
+
 public abstract class Conta {
     
     int numeroAgencia;
     String numeroConta;
     String CPF;
+    String email;
+    int ddd;
+    String numeroCelular;
     String nomeTitular;
     String senhaTitular;
-    String dataNascimento;
-    String dataCriacaoAcc;
+    Date dataNascimento;
+    Date dataCriacaoAcc;
     double saldo;
     byte tipo;
-    
 }
--- src/com/bancodelt/java/models/ContaPoupanca.java
+++ src/com/bancodelt/java/models/ContaPoupanca.java
@@ -1,14 +1,19 @@
 package com.bancodelt.java.models;
 
+import java.util.Date;
+
 public class ContaPoupanca extends Conta {
 
     ContaPoupanca() {
     }
     
-    ContaPoupanca(int numeroAgencia, String numeroConta, String CPF, String nomeTitular, String senhaTitular, String dataNascimento, String dataCriacaoAcc, double saldo) {
+    ContaPoupanca(int numeroAgencia, String numeroConta, String CPF, String email, int ddd, String numeroCelular, String nomeTitular, String senhaTitular, Date dataNascimento, Date dataCriacaoAcc, double saldo) {
         this.numeroAgencia = numeroAgencia;
         this.numeroConta = numeroConta;
         this.CPF = CPF;
+        this.email = email;
+        this.ddd = ddd;
+        this.numeroCelular = numeroCelular;
         this.nomeTitular = nomeTitular;
         this.senhaTitular = senhaTitular;
         this.dataNascimento = dataNascimento;
