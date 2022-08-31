885bc39a4a04cebd4e0a7218a2e2ab1e1ba4391e
JoaoRibas041 <ribas.joao1410@gmail.com>
2022-08-30 23:22:51 -0300

    Alteração dos FXML

--- src/com/bancodelt/java/program/controllers/ViewCadastrarAppController.java
+++ src/com/bancodelt/java/program/controllers/ViewCadastrarAppController.java
@@ -8,6 +8,8 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
@@ -17,8 +19,6 @@ public class ViewCadastrarAppController implements Initializable {
     @FXML
     private AnchorPane TelaCriarAccApp;
     @FXML
-    private Button btnEntrar;
-    @FXML
     private TextField txtFCPF;
     @FXML
     private TextField txtFNome;
@@ -26,7 +26,19 @@ public class ViewCadastrarAppController implements Initializable {
     private TextField txtFNascimento;
     @FXML
     private ComboBox<?> cbEstiloDaConta;
-
+    @FXML
+    private Button btnCriarAcc;
+    @FXML
+    private DatePicker dpNascimento;
+    @FXML
+    private TextField txtFSenha;
+    @FXML
+    private TextField txtFConfirmSenha;
+    @FXML
+    private Label txtFSenha1;
+    @FXML
+    private TextField txtFEmail;
+    
     Main m = new Main();
     
     @Override
@@ -40,8 +52,10 @@ public class ViewCadastrarAppController implements Initializable {
     }
     
     private void CheckVariaveis() throws Exception {
-        Main.getProgram().close();
-        m.rodarTela(new Stage(), "ViewPrincipalApp.fxml");
+        
+        
+        //Main.getProgram().close();
+        //m.rodarTela(new Stage(), "ViewPrincipalApp.fxml");
     }
     
 }
--- src/com/bancodelt/java/program/controllers/ViewOpenAppController.java
+++ src/com/bancodelt/java/program/controllers/ViewOpenAppController.java
@@ -10,17 +10,21 @@ import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 public class ViewOpenAppController implements Initializable {
-
+    
+    @FXML
+    private AnchorPane TelaPrincipalApp;
     @FXML
     private Button btnEntrar;
     @FXML
     private TextField txtFCPF;
-    @FXML
-    private AnchorPane TelaPrincipalApp;
+    
+    public String CPFinput;
     
     Main m = new Main();
     AlertWarningPrototype alertaAviso = new AlertWarningPrototype();
@@ -36,15 +40,39 @@ public class ViewOpenAppController implements Initializable {
         CheckACC();
     }
     
-    private void CheckACC() throws Exception {            
+    @FXML
+    private void acaoBtnEnter(KeyEvent e) throws Exception {
+        if(e.getCode() == KeyCode.ENTER){
+            CheckACC();
+        }
+    }
+
+    @FXML
+    private void acaoTXTFEnter(KeyEvent e) throws Exception {
+        if(e.getCode() == KeyCode.ENTER){
+            CheckACC();
+        }
+    }
+    
+        private void CheckACC() throws Exception {            
         if (txtFCPF.getText().isEmpty()) {
             alertaAviso.setTitulo("Alerta");
             alertaAviso.setMsgPrincipal("Informe o CPF!");
             alertaAviso.setSubMsg("O campo CPF esta vazio. Por favor informe o CPF");
             alertaAviso.exibirAlert();
         } else {
-            Main.getProgram().close();
-            m.rodarTela(new Stage(), "ViewCadastrarApp.fxml");
+            CPFinput = txtFCPF.getText();
+            
+            // se conta existe, entrar direto, caso contrario ir para criar conta
+            if(CPFinput.equalsIgnoreCase("138")) {
+                System.out.println("CPF VALIDO, entrando...");
+                // criar txtF para senha e mostrar txtF caso conta ja exista
+                // ou fazer menu personalizado para login de conta existente
+            } else {
+                System.out.println("CPF INVALIDO, criar conta");
+                Main.getProgram().close();
+                m.rodarTela(new Stage(), "ViewCadastrarApp.fxml");
+            }
         }
     }
 }
