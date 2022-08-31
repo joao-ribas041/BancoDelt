52fdfae499422a770c68de16f51d808f84e4c331
JoaoRibas041 <ribas.joao1410@gmail.com>
2022-08-31 00:08:14 -0300

    Metodo sacar e depositar na classe abstrata

--- src/com/bancodelt/java/models/Conta.java
+++ src/com/bancodelt/java/models/Conta.java
@@ -16,4 +16,12 @@ public abstract class Conta {
     Date dataCriacaoAcc;
     double saldo;
     byte tipo;
+    
+    void sacar() {
+        
+    }
+    
+    void depositar() {
+        
+    }
 }
