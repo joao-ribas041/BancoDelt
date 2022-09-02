1a521bb1a2f425b697013021b8c38598af594323
JoaoRibas041 <ribas.joao1410@gmail.com>
2022-09-01 21:04:01 -0300

    alteração no controller e no fxml do cadastro

--- src/com/bancodelt/java/program/controllers/ViewCadastrarAppController.java
+++ src/com/bancodelt/java/program/controllers/ViewCadastrarAppController.java
@@ -1,10 +1,12 @@
 package com.bancodelt.java.program.controllers;
 
+import com.bancodelt.java.models.ContaCorrente;
 import com.bancodelt.java.models.EstiloAcc;
 import com.bancodelt.java.models.alerts.AlertWarningPrototype;
 import com.bancodelt.java.program.Main;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import java.util.ResourceBundle;
 import javafx.collections.FXCollections;
@@ -30,8 +32,6 @@ public class ViewCadastrarAppController implements Initializable {
     @FXML
     private TextField txtFNome;
     @FXML
-    private TextField txtFNascimento;
-    @FXML
     private Button btnCriarAcc;
     @FXML
     private DatePicker dpNascimento;
@@ -42,17 +42,23 @@ public class ViewCadastrarAppController implements Initializable {
     @FXML
     private TextField txtFEmail;
     @FXML
+    private TextField txtFddd;
+    @FXML
+    private TextField txtFnumeroTelefone;
+    @FXML
     private ComboBox<EstiloAcc> cbEstiloDaConta;
     private List<EstiloAcc> estilosAcc = new ArrayList<>();
     private ObservableList<EstiloAcc> obsEstiloAcc;
     
+    private int numeroAgencia, ddd;
+    private String numeroConta, CPF, email, numeroCelular, nomeTitular;
+    private Date dataNascimento, dataCriacaoAcc;
+    private double saldo;
+    private byte tipo;
+    
     Main m = new Main();
     ViewOpenAppController voac = new ViewOpenAppController();
     AlertWarningPrototype alertaAviso;
-    @FXML
-    private TextField txtFddd;
-    @FXML
-    private TextField txtFnumeroTelefone;
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
@@ -101,14 +107,26 @@ public class ViewCadastrarAppController implements Initializable {
             CheckVariaveis();
         }
     }
-    
-    
+    @FXML
+    private void cbEstiloEnter(KeyEvent e) throws Exception {
+        if(e.getCode() == KeyCode.ENTER) {
+            CheckVariaveis();
+        }
+    }
+    @FXML
+    private void dpDataEnter(KeyEvent e) throws Exception {
+        if(e.getCode() == KeyCode.ENTER) {
+            CheckVariaveis();
+        }
+    }
     
     private void CheckVariaveis() throws Exception {
         if(txtFEmail.getText().isEmpty()){
             alertaAviso = new AlertWarningPrototype("Alerta", "Informe o E-mail!", "O campo E-mail está vazio, por favor informe-o.");
         } else {
             if(txtFEmail.getText().contains("@") && txtFEmail.getText().contains(".com")) {
+                email = txtFEmail.getText();
+                
                 
                 if(txtFddd.getText().isEmpty()) {
                     alertaAviso = new AlertWarningPrototype("Alerta", "Informe o seu DDD", "O campo DDD está vazio, por favor informe-o.");
@@ -118,25 +136,58 @@ public class ViewCadastrarAppController implements Initializable {
                         alertaAviso = new AlertWarningPrototype("Alerta", "Informe o seu numero", "O campo numero de telefone está vazio, por favor informe-o.");
                     } else {
                         
+                        
                         if(txtFNome.getText().isEmpty()) {
                             alertaAviso = new AlertWarningPrototype("Alerta", "Informe o seu nome", "O campo nome está vazio, por favor informe-o.");
                         } else {
                             
                             if(txtFNome.getText().length() < 3 || txtFNome.getText().length() >= 50) {
-                                alertaAviso = new AlertWarningPrototype("Alerta", "Nome invalido", "O nome informado é invalido, por favor informe um nome legitmo.");
                                 System.out.println("");
                                 System.out.println("Menor que 3 e maior que 50");
+                                alertaAviso = new AlertWarningPrototype("Alerta", "Nome invalido", "O nome informado é invalido, por favor informe um nome legitmo.");
                             } else {
                                 
-                                //implementar if da data
-                                
-                                if(txtFSenha.getText().isEmpty() || txtFConfirmSenha.getText().isEmpty()){
-                                    alertaAviso = new AlertWarningPrototype("Alerta", "Informe a senha", "O campo senha está vazio, por favor informe-o.");
+                                if(dpNascimento.getValue() != null) {
+                                    
+                                    if(txtFSenha.getText().isEmpty() || txtFConfirmSenha.getText().isEmpty()) {
+                                        alertaAviso = new AlertWarningPrototype("Alerta", "Informe a senha", "O campo senha está vazio, por favor informe-o.");
+                                    } else {
+                                        if(txtFSenha.getText().equals(txtFConfirmSenha.getText())) {
+                                            System.out.println("Senhas iguais");
+                                            
+                                            if(cbEstiloDaConta.getValue() != null) {
+                                                
+                                                
+                                                if(cbEstiloDaConta.getValue().getEstilo().equals("Conta Corrente")) {
+                                                    System.out.println("");
+                                                    System.out.println("Conta corrente");
+                                                    System.out.println("");
+                                                    ContaCorrente cc;
+                                                    cc = new ContaCorrente();
+                                                    
+                                                }
+                                                if(cbEstiloDaConta.getValue().getEstilo().equals("Conta Poupança")) {
+                                                    System.out.println("");
+                                                    System.out.println("Conta poupança");
+                                                    System.out.println("");
+                                                }
+                                                
+                                            } else {
+                                                alertaAviso = new AlertWarningPrototype("Alerta", "Informe o estilo da conta", "Você não escolheu o estilo da conta, por favor informe-o.");
+                                            }
+                                            
+                                            
+                                        } else {
+                                            System.out.println("senhas diferentes");
+                                        }
+                                    }
+                                    
                                 } else {
-                                    
-                                    
+                                    alertaAviso = new AlertWarningPrototype("Alerta", "Data invalida", "A data informada é invalido, por favor informe uma data legitma.");
                                     
                                 }
+                                
+                                
                                 
                             }
                             
