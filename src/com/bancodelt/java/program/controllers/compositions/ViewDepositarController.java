c254111d48036e22351b1a3084ef0a75cca62b55
JoaoRibas041 <ribas.joao1410@gmail.com>
2022-09-02 20:43:46 -0300

    TELAS APP
    Inicio da criação das telas do aplicativo

---
+++ src/com/bancodelt/java/program/controllers/compositions/ViewDepositarController.java
@@ -0,0 +1,12 @@
+package com.bancodelt.java.program.controllers.compositions;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+public class ViewDepositarController implements Initializable {
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+}
---
+++ src/com/bancodelt/java/program/controllers/compositions/ViewSacarController.java
@@ -0,0 +1,13 @@
+package com.bancodelt.java.program.controllers.compositions;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+public class ViewSacarController implements Initializable {
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }
+}
